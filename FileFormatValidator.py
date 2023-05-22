296e64fc25eed4210ad08dadc405e09fc9952a3c
Mohammed Ismail <mohammedismail523261@gmail.com>
2023-05-23 01:53:23 +0530

    subject related updates

--- FileFormatValidator.py
+++ FileFormatValidator.py
@@ -13,8 +13,9 @@ def validate_file_format(file_path):
             return
 
         # Define the master data for dimension values and metric value range
-        master_dimensions = ["Accountancy", "Biology", "Business Studies", "Chemistry", "English"]
-        metric_value_range = (0, 100)
+        master_dimensions = ["Accountancy", "Business Studies", "Civics","Creative Writing and Translation","Home Science","Health and Physical Education" ,"EVS","Urdu","Sanskrit","Sociology","Computer Science","SCIENCE","Sanskrit workbook","English", "Biology", "Chemistry", "Mathematics", "Physics", "Geography", "Urdu", "History", "Economics", "Computer Science", "Political Science", "Sociology", "Psychology", "Physical Education", "Fine Arts","HINDI","ENGLISH","Statistics","MATHS",""]
+
+        metric_value_range = (0, 1000)
 
         # Validate each row
         row_number = 2  # Start from row 2 since we already read the header
@@ -33,12 +34,13 @@ def validate_file_format(file_path):
                 # Format check: Example format checks
                 if not tb_id.startswith("do_"):
                     print(f"Error: Invalid TB Id format in row {row_number}.")
-                if medium not in ["ENGLISH", "MATHS", "SCIENCE"]:
+                if medium not in ["ENGLISH", "MATHS", "SCIENCE", "HINDI","URDU","SANSKRIT","Urdu","Sanskrit"]:
                     print(f"Error: Invalid Medium value in row {row_number}.")
                 if not grade.startswith("Class"):
                     print(f"Error: Invalid Grade format in row {row_number}.")
-                if subject.isdigit():
-                    print(f"Error: Invalid Subject value in row {row_number}. Subject cannot be a number.")
+                if not subject.lower() in [dimension.lower() for dimension in master_dimensions] and  not subject.upper() in [dimension.upper() for dimension in master_dimensions] and subject not in master_dimensions:
+                    print(f"Error: Invalid Subject value in row {row_number}. Subject does not match the master data.")
+
                 if not linked_qr_count.isdigit():
                     print(f"Error: Invalid Linked QR Count value in row {row_number}. Must be a non-negative integer.")
                 if not resource_count.isdigit():
@@ -49,10 +51,6 @@ def validate_file_format(file_path):
                     print(f"Error: Missing values in row {row_number}. All fields are required.")
 
                 # Accuracy check: Add your accuracy checks here
-
-                # Dimension values check: Validate against master data
-                if subject not in master_dimensions:
-                    print(f"Error: Invalid Subject value in row {row_number}. Subject does not match the master data.")
 
                 # Metric values check: Validate within range
                 if not metric_value_range[0] <= int(linked_qr_count) <= metric_value_range[1]:
